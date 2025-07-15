707d3d67935b0a213e7621d4c27962050ad9c596
pingweiii <pwchen31@gmail.com>
2025-07-16 01:39:40 +0800

    refactor(login): adjusted the spacing of the codes

--- pages/login/index.php
+++ pages/login/index.php
@@ -5,19 +5,19 @@ $error = $_GET['error'] ?? '';
 <div class="login-container">
   <div class="parchment-login">
     <h2 class="login-title">MineForge Access</h2>
-    <div class="login-divider">⚒⚒⚒</div>
+    <div class = "login-divider">⚒⚒⚒</div>
 
-    <form action="/handlers/auth.handler.php" method="POST" class="login-form">
-      <div class="form-group">
-        <label>Username: </label>
-        <input type="text" name="username" required>
-      </div>
 
-      <div class="form-group">
-        <label>Password: </label>
-        <input type="password" id="password" name="password" class="form-input" required>
-      </div>
+<form action="/handlers/auth.handler.php" method="POST" class="login-form">
+  <div class="form-group">
+    <label>Username: </label>
+    <input type="text" name="username" required>
+  </div>
 
+  <div class="form-group">
+    <label>Password: </label>
+    <input type="password" id="password" name="password" class="form-input" required>
+  </div>
       <input type="hidden" name="action" value="login">
       <button type="submit" class="login-btn">Enter The Mines</button>
 
