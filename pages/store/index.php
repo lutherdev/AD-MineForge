ff1f158e88ae94453042ac3dba166e00c0bc41b7
pingweiii <pwchen31@gmail.com>
2025-07-16 02:36:25 +0800

    feat(store): added the base layout of the store page

---
+++ pages/store/index.php
@@ -0,0 +1,49 @@
+<section class="store-container" id="inventory">
+  <div class="store-header">
+    <h1>Miner's Ware</h1>
+      <div class="leftside">
+        <a href='/topup'><button class="topupbtn">TOP UP</button></a>
+        <a href='/view-orders'><button class="ordersbtn">YOUR ORDERS</button></a>
+        <div class="walet">
+          <h1>Wallet: gold</h1>
+        </div>
+      </div>
+    <div class="store-filters">
+    </div>
+  </div>
+
+  <div class="store-main">
+    <div class="sidebar">
+      <div class="cart-image-wrapper desktop-cart">
+        <aside class="cart-box">
+          <h2>Cart</h2>
+          <ul id="cart-items"></ul>
+          <div class="total">Total: ₱<span id="cart-total">0</span></div>
+          <div class='cart-buttons'>
+            <div class='btn-cancel'><button id="checkout-btn">CHECKOUT</button></div>
+            <div class='btn-cancel'><button id="cancel-btn">CANCEL</button></div>
+          </div>
+        </aside>
+      </div>
+    </div>
+
+    <div class="products-image-wrapper">
+      <div class="products-grid">
+          <div class="product-card">
+          <img src="">
+          <div class="product-info">
+            <h3></h3>
+            <p></p>
+            <p> PIECES LEFT</p>
+            <div class="price-action">
+              <span class="price">₱</span>
+              <button onclick="">Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</section>
+<script src="/pages/store/assets/js/store.js"></script>
