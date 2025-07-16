1f9bbe1f7988a983a52c71ba69e8df77a4b115ea
pingweiii <pwchen31@gmail.com>
2025-07-16 15:38:17 +0800

    feat(profile page): added the layout of the profile page

---
+++ pages/profile-page/index.php
@@ -0,0 +1,32 @@
+<!--BACKEND-->
+<!--BACKEND-->
+<!--BACKEND-->
+<!--BACKEND-->
+<!--BACKEND-->
+
+
+<div class="profile-outer">
+  <div class="profile-card">
+    <h2>Your Profile</h2>
+    <p><strong>Username:</strong> <!--BACKEND--></p>
+    <p><strong>Name:</strong> <!--BACKEND--></p>
+    <p><strong>Role:</strong> <!--BACKEND--></p>
+    <p><strong>City:</strong> <!--BACKEND--></p>
+    <p><strong>Province:</strong> <!--BACKEND--></p>
+    <p><strong>Street:</strong> <!--BACKEND--></p>
+    <p><strong>Wallet:</strong> <!--BACKEND--> gold</p>
+
+    <div class="profile-buttons">
+      <a href="topup"><button class="gold-btn">Top Up</button></a>
+      <a href="edit-profile"><button class="gold-btn">Edit Profile</button></a>
+      <a href="view-orders"><button class="gold-btn">View Orders</button></a>
+      <!--BACKEND-->
+      <!--BACKEND-->
+      <!--BACKEND-->
+      <!--BACKEND-->
+      <!--BACKEND-->
+      <!--BACKEND-->
+      
+    </div>
+  </div>
+</div>
