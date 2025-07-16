f2669f266b1109b536e756bcb837922d0b77d427
pingweiii <pwchen31@gmail.com>
2025-07-16 16:05:27 +0800

    feat(view orders page): added code for the layout of the view orders page

---
+++ pages/view-orders/index.php
@@ -0,0 +1,44 @@
+<!--BACKEND-->
+<!--BACKEND-->
+<!--BACKEND-->
+<!--BACKEND-->
+<!--BACKEND-->
+
+<div class="view-outer">
+  <div class="in-outer">
+    <div class="view">
+      <h2>Your Orders</h2>
+    </div>
+
+    <div class="table-container">
+      <!--BACKEND-->
+      <!--BACKEND-->
+      <!--BACKEND-->
+        <div class="scroll-wrapper">
+          <table class="user-table">
+            <thead>
+              <tr>
+                <th>Order ID</th>
+                <th>Date</th>
+                <th>Item</th>
+                <th>Quantity</th>
+                <th>Total (Gold)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!--BACKEND-->
+                <tr>
+                  <td><!--BACKEND--><!--BACKEND--></td>
+                  <td><!--BACKEND--><!--BACKEND--></td>
+                  <td><!--BACKEND--><!--BACKEND--></td>
+                  <td><!--BACKEND--><!--BACKEND--></td>
+                  <td><!--BACKEND--><!--BACKEND--></td>
+                </tr>
+              <!--BACKEND--><!--BACKEND-->
+            </tbody>
+          </table>
+        </div>
+      <!--BACKEND--><!--BACKEND-->
+    </div>
+  </div>
+</div>
