0164c3f1a33576ee413f46cb7311ba41738d989d
lutherdev <devturd@gmail.com>
2025-07-16 12:54:10 +0800

    feat (register): added connection to database that inserts successful register

--- utils/users.util.php
+++ utils/users.util.php
@@ -5,3 +5,21 @@ function getUserData($username): array {
     $stmt->execute([':username' => $username]);
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
+
+function insertUser($pdo, $user) {
+    $stmt = $pdo->prepare("
+        INSERT INTO users (username, password, first_name, last_name, street, city, province, role)
+        VALUES (:username, :password, :first_name, :last_name, :street, :city, :province, :role)
+    ");
+    
+    $stmt->execute([
+        ':username' => $user['username'],
+        ':first_name'       => strtolower($user['first_name']),
+        ':last_name'       => strtolower($user['last_name']),
+        ':password'       => password_hash($user['password'], PASSWORD_DEFAULT),
+        ':role' => $user['username'] === 'Notch' ? 'admin' : 'customer',
+        ':street' => strtolower($user['street']),
+        ':province' => strtolower($user['province']),
+        ':city' => strtolower($user['city']),
+    ]);
+}
