c3806bd788f6e72eacd06c42512341508e02fbd7
lutherdev <devturd@gmail.com>
2025-07-16 12:53:11 +0800

    feat (register): added validators for password inputs

--- utils/validators.util.php
+++ utils/validators.util.php
@@ -3,11 +3,19 @@ function validateUsername(string $username): string|true {
     $username = trim($username);
 
     if (preg_match('/\s/', $username)) {
-        return 'invalid_username'; // contains spaces
+        return 'invalid_username';
     }
 
     if (!preg_match('/^[a-zA-Z0-9_]{3,20}$/', $username)) {
-        return 'invalid_username'; // invalid format
+        return 'invalid_username';
+    }
+
+    return true;
+}
+
+function validatePassword(string $password): string|true {
+    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/', $password)) {
+        return 'weak_password';
     }
 
     return true;
