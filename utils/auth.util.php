11738c4db26e3e04505b45c739c582316e201963
lutherdev <devturd@gmail.com>
2025-07-16 12:39:38 +0800

    feat (auth util): added logout function

--- utils/auth.util.php
+++ utils/auth.util.php
@@ -33,6 +33,24 @@ class Auth{
             return 'db_error';
         }
     }
+    
+    public static function logout(): void
+    {
+        $_SESSION = [];
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+        session_destroy();
+    }
 
 
     public static function user(): ?array
