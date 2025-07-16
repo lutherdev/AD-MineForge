ca2e2a55dab2be318e9f04241be208b1e443416f
lutherdev <devturd@gmail.com>
2025-07-16 15:26:15 +0800

    feat (store): added a function that gets all available items

---
+++ utils/items.util.php
@@ -0,0 +1,10 @@
+<?php
+function getAvailableItems(): array {
+    global $pdo;
+    $stmt = $pdo->prepare("SELECT * FROM items WHERE status = 'is-active'");
+    $stmt->execute();
+    $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $items;
+}
+
